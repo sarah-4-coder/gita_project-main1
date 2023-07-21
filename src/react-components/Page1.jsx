e762f8c8c1cef8d0a376e8a80df908e00ea66f5d
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-07-21 23:50:07 +0530

    video fixed

--- src/react-components/Page1.jsx
+++ src/react-components/Page1.jsx
@@ -4,9 +4,10 @@ import Search from "./Search";
 const Page1 = () => {
   return (
     <div className="page1__section">
-      <div className="page1__section--vid fixed top-0 left-0 w-screen h-screen">
-        <video loop autoPlay className="w-full h-full object-cover">
+      <div>
+        <video autoPlay muted loop id="myVideo" className='sm:w-[100vw] sm:h-[100vh] h-[60vh] object-cover opacity-100  top-0 -z-50 relative' >
           <source src={vid2} type="video/mp4" />
+          Your browser does not support HTML5 video.
         </video>
       </div>
       <form className="d-flex page__form" role="search">
