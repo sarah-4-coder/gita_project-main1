8eec7cc6d5e53895e13c93cb9d17b5c3a9770da7
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-07-22 00:18:36 +0530

    speech to text done

--- src/react-components/Search.jsx
+++ src/react-components/Search.jsx
@@ -1,11 +1,104 @@
+// import React, { useState, useEffect } from "react";
+// // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// // import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
+
+// const SearchBar = () => {
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [listening, setListening] = useState(false);
+//   const [recordingTime, setRecordingTime] = useState(0);
+
+//   useEffect(() => {
+//     let timerId;
+
+//     if (listening) {
+//       timerId = setInterval(() => {
+//         setRecordingTime((prevTime) => prevTime + 1);
+//       }, 5000);
+//     }
+
+//     return () => {
+//       clearInterval(timerId);
+//     };
+//   }, [listening]);
+
+//   const handleKeyDown = (event) => {
+//     if (event.key === "Enter") {
+//       handleSubmit();
+//     }
+//   };
+
+//   const handleMicClick = () => {
+//     if (!listening) {
+//       startListening();
+//     } else {
+//       stopListening();
+//     }
+//   };
+
+//   const startListening = () => {
+//     setListening(true);
+//     setRecordingTime(0);
+
+//     const recognition = new window.webkitSpeechRecognition();
+//     recognition.continuous = false;
+//     recognition.interimResults = false;
+
+//     recognition.onresult = (event) => {
+//       const transcript = event.results[0][0].transcript;
+//       setSearchTerm(transcript);
+//       recognition.stop();
+//       handleSubmit();
+//     };
+
+//     recognition.start();
+//   };
+
+//   const stopListening = () => {
+//     setListening(false);
+//   };
+
+//   const handleSubmit = () => {
+//     // Perform the necessary backend API request to fetch results based on the searchTerm
+//     fetch(`/api/search?term=${searchTerm}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         // Process the search results data
+//         console.log("Search results:", data.results);
+//       })
+//       .catch((error) => {
+//         console.error("Error searching:", error);
+//       });
+//   };
+
+//   return (
+//     <div className="search-bar">
+//       <input
+//         type="text"
+//         value={searchTerm}
+//         onChange={(e) => setSearchTerm(e.target.value)}
+//         onKeyDown={handleKeyDown}
+//         placeholder="Type Your Question Here!"
+//       />
+//       <button
+//         className={`microphone-button ${listening ? "listening" : ""}`}
+//         onClick={handleMicClick}
+//       >
+//         <i class="fa-solid fa-microphone" style={{ color: "#000000" }}></i>
+//       </button>
+//       {listening && <span className="recording-time">{recordingTime}s</span>}
+//     </div>
+//   );
+// };
+
+// export default SearchBar;
+
 import React, { useState, useEffect } from "react";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [listening, setListening] = useState(false);
   const [recordingTime, setRecordingTime] = useState(0);
+  const [micInputComplete, setMicInputComplete] = useState(false);
 
   useEffect(() => {
     let timerId;
@@ -13,7 +106,7 @@ const SearchBar = () => {
     if (listening) {
       timerId = setInterval(() => {
         setRecordingTime((prevTime) => prevTime + 1);
-      }, 5000);
+      }, 1000); // Reduce interval for more accurate recording time display
     }
 
     return () => {
@@ -27,8 +120,9 @@ const SearchBar = () => {
     }
   };
 
-  const handleMicClick = () => {
-    if (!listening) {
+  const handleMicClick = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+    if (!listening && !micInputComplete) {
       startListening();
     } else {
       stopListening();
@@ -38,6 +132,7 @@ const SearchBar = () => {
   const startListening = () => {
     setListening(true);
     setRecordingTime(0);
+    setMicInputComplete(false);
 
     const recognition = new window.webkitSpeechRecognition();
     recognition.continuous = false;
@@ -47,7 +142,14 @@ const SearchBar = () => {
       const transcript = event.results[0][0].transcript;
       setSearchTerm(transcript);
       recognition.stop();
-      handleSubmit();
+      setMicInputComplete(true); // Indicate that microphone input is complete
+    };
+
+    recognition.onend = () => {
+      if (!micInputComplete) {
+        // If the microphone input was not complete, restart the recognition
+        recognition.start();
+      }
     };
 
     recognition.start();
@@ -55,6 +157,7 @@ const SearchBar = () => {
 
   const stopListening = () => {
     setListening(false);
+    setMicInputComplete(true); // Indicate that the user manually stopped the microphone input
   };
 
   const handleSubmit = () => {
@@ -83,9 +186,15 @@ const SearchBar = () => {
         className={`microphone-button ${listening ? "listening" : ""}`}
         onClick={handleMicClick}
       >
-        <i class="fa-solid fa-microphone" style={{ color: "#000000" }}></i>
+        {listening && !micInputComplete ? (
+          <i className="fa-solid fa-microphone" style={{ color: "#ff0000" }}></i>
+        ) : (
+          <i className="fa-solid fa-microphone" style={{ color: "#000000" }}></i>
+        )}
       </button>
-      {listening && <span className="recording-time">{recordingTime}s</span>}
+      {listening && !micInputComplete && (
+        <span className="recording-time">{recordingTime}s</span>
+      )}
     </div>
   );
 };
