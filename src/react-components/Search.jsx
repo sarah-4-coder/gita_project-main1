cd3f283b71ea182c547d4f63970eb340abb29ce8
sarah-4-coder <rsparshrai@gmail.com>
2023-07-22 00:31:11 +0530

    3 commit

--- src/react-components/Search.jsx
+++ src/react-components/Search.jsx
@@ -1,91 +1,106 @@
-// import React, { useState, useEffect } from "react";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
 
 const SearchBar = () => {
-  // const [searchTerm, setSearchTerm] = useState("");
-  // const [listening, setListening] = useState(false);
-  // const [recordingTime, setRecordingTime] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [listening, setListening] = useState(false);
+  const [recordingTime, setRecordingTime] = useState(0);
+  const [micInputComplete, setMicInputComplete] = useState(false);
 
-  // useEffect(() => {
-  //   let timerId;
+  useEffect(() => {
+    let timerId;
 
-  //   if (listening) {
-  //     timerId = setInterval(() => {
-  //       setRecordingTime((prevTime) => prevTime + 1);
-  //     }, 5000);
-  //   }
+    if (listening) {
+      timerId = setInterval(() => {
+        setRecordingTime((prevTime) => prevTime + 1);
+      }, 1000); // Reduce interval for more accurate recording time display
+    }
 
-  //   return () => {
-  //     clearInterval(timerId);
-  //   };
-  // }, [listening]);
-
-  // const handleKeyDown = (event) => {
-  //   if (event.key === "Enter") {
-  //     handleSubmit();
-  //   }
-  // };
-
-  // const handleMicClick = () => {
-  //   if (!listening) {
-  //     startListening();
-  //   } else {
-  //     stopListening();
-  //   }
-  // };
-
-  // const startListening = () => {
-  //   setListening(true);
-  //   setRecordingTime(0);
-
-  //   const recognition = new window.webkitSpeechRecognition();
-  //   recognition.continuous = false;
-  //   recognition.interimResults = false;
-
-  //   recognition.onresult = (event) => {
-  //     const transcript = event.results[0][0].transcript;
-  //     setSearchTerm(transcript);
-  //     recognition.stop();
-  //     handleSubmit();
-  //   };
-
-  //   recognition.start();
-  // };
-
-  // const stopListening = () => {
-  //   setListening(false);
-  // };
-
-  // const handleSubmit = () => {
-  //   // Perform the necessary backend API request to fetch results based on the searchTerm
-  //   fetch(`/api/search?term=${searchTerm}`)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       // Process the search results data
-  //       console.log("Search results:", data.results);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error searching:", error);
-  //     });
-  // };
-
-  const handleclick = () => {
-    var recognition = new webkitSpeechRecognition();
-    recognition.lang = "en-GB";
-    recognition.onresult = function (event) {
-      console.log(event);
-      document.getElementById("stt").value = event.results[0][0].transcript;
+    return () => {
+      clearInterval(timerId);
     };
+  }, [listening]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  const handleMicClick = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+    if (!listening && !micInputComplete) {
+      startListening();
+    } else {
+      stopListening();
+    }
+  };
+
+  const startListening = () => {
+    setListening(true);
+    setRecordingTime(0);
+    setMicInputComplete(false);
+
+    const recognition = new window.webkitSpeechRecognition();
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    recognition.onresult = (event) => {
+      const transcript = event.results[0][0].transcript;
+      setSearchTerm(transcript);
+      recognition.stop();
+      setMicInputComplete(true); // Indicate that microphone input is complete
+    };
+
+    recognition.onend = () => {
+      if (!micInputComplete) {
+        // If the microphone input was not complete, restart the recognition
+        recognition.start();
+      }
+    };
+
     recognition.start();
+  };
+
+  const stopListening = () => {
+    setListening(false);
+    setMicInputComplete(true); // Indicate that the user manually stopped the microphone input
+  };
+
+  const handleSubmit = () => {
+    // Perform the necessary backend API request to fetch results based on the searchTerm
+    fetch(`/api/search?term=${searchTerm}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Process the search results data
+        console.log("Search results:", data.results);
+      })
+      .catch((error) => {
+        console.error("Error searching:", error);
+      });
   };
 
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Type Your Question Here!" id="stt" />
-      <button onClick={handleclick}>
-        <i class="fa-solid fa-microphone" style={{ color: "#000000" }}></i>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={handleKeyDown}
+        placeholder="Type Your Question Here!"
+      />
+      <button
+        className={`microphone-button ${listening ? "listening" : ""}`}
+        onClick={handleMicClick}
+      >
+        {listening && !micInputComplete ? (
+          <i className="fa-solid fa-microphone" style={{ color: "#ff0000" }}></i>
+        ) : (
+          <i className="fa-solid fa-microphone" style={{ color: "#000000" }}></i>
+        )}
       </button>
+      {listening && !micInputComplete && (
+        <span className="recording-time">{recordingTime}s</span>
+      )}
     </div>
   );
 };
