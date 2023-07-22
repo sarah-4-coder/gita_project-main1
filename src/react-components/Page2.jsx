d7eb8922b455b1daf818f2c3fed32f4210801781
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-07-22 12:43:48 +0530

    page 2 fixed

--- src/react-components/Page2.jsx
+++ src/react-components/Page2.jsx
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from "react";
 import Main from "../assets/main2.jpg";
 import Scroll from "../assets/scroll2.gif";
 import NewImage from "../assets/new_scroll.png";
-import Typewriter from "typewriter-effect";
 import Pen from "../assets/pen.png";
 
 export default function Home() {
   const [scrollImage, setScrollImage] = useState(Scroll);
+  const initialDisplayText =
+    "lorem*12asdhasudhas dh asu9od hauodhuodi huias hduiasasus uas yuisdg huisg hau duio ad suios uiod huiobdauisgbyuiasg uias nduiasuiao ";
   const [displayText, setDisplayText] = useState("");
+  const [textIndex, setTextIndex] = useState(0);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setScrollImage(NewImage);
-      setDisplayText(
-        "In publishing and graphic design, Lorem.ftdfghdhgdghdthdtdthdhfdrdfgdrdfxtdxfdthdhxtmdxfgdrtfthfhgftghdcgfdxgfdhfdchfdhfdxgrfs"
-      );
+      startTextAnimation();
     }, 3400);
 
     return () => {
@@ -27,22 +27,26 @@ export default function Home() {
     cursorElement.style.cursor = `url(${Pen}), auto`;
   }, []);
 
+  const startTextAnimation = () => {
+    const interval = setInterval(() => {
+      setTextIndex((prevIndex) => {
+        if (prevIndex >= initialDisplayText.length) {
+          clearInterval(interval);
+          return prevIndex;
+        } else {
+          setDisplayText(initialDisplayText.substring(0, prevIndex + 1));
+          return prevIndex + 1;
+        }
+      });
+    }, 40);
+  };
+
   return (
     <div className="custom-cursor">
       <div className="h-screen w-screen flex flex-col-reverse md:flex-row">
         <div className="flex basis-1/2 h-screen md:h-max-[60%] lg:h-max-[100%] align-middle justify-center my-auto relative">
           <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-            <div className="text-black  z-20   ">
-              <Typewriter
-                options={{
-                  strings: [displayText],
-                  autoStart: true,
-                  loop: true,
-                  typeSpeed: 100,
-                  deleteSpeed: 0,
-                }}
-              />
-            </div>
+            <div className="text-black z-20 w-[40%] text-center text-2xl">{displayText}</div>
           </div>
           <img
             alt=""
