ed3af0d8389eb8b0734d1369a166043d4c950020
sarah-4-coder <rsparshrai@gmail.com>
2023-07-24 03:20:45 +0530

    all dne

--- src/react-components/Page2.jsx
+++ src/react-components/Page2.jsx
@@ -34,7 +34,7 @@ export default function Home() {
         <img alt="" src={Main} className="god__img" />
       </div>
       <div className="h-screen  w-screen lg:flex  lg:flex-row ">
-        <div className=" flex basis-1/2 h-[75vh] md:h-max-[60%] lg:h-screen align-middle justify-center my-auto relative top-[25%] lg:top-0">
+        <div className=" scontent  flex basis-1/2 h-[75vh] md:h-max-[60%] lg:h-screen align-middle justify-center my-auto relative top-[25%] lg:top-0">
           <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-black  z-20">
             <TypewriterEffect texts={displayText} />
           </div>
